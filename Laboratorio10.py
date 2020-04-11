346e7d4a23648395020f496c335575f29868393f
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2020-04-11 15:54:21 -0500

    solucion de ejercicio 9.1

--- Laboratorio10.py
+++ Laboratorio10.py
@@ -5,16 +5,19 @@ Created on Tue Apr  7 20:22:08 2020
 @author: abaly
 """
 
-
-#%%ejercicio 1
-def a_power_b (): #creo una funcion para desarrolar el ejericio
-    a=int(input("ingrese el valor de a: ")) #aqui pregunto por a
-    b=int(input("ingrese el valor de b: "))#aqui pregunto por b
+print("ingrese 0 en a para terminar")  
+def a_power_b (a,b): #creo una funcion para desarrolar el ejericio
     d = 1  #me ayuda en el proceso
     e = 1 #en esta variable voy almacenando las multiplicaciones. 
     while d <= b: #condicional para que el algoritmo sepa donde detenerse
         e = e * a #aqui le voy agregando los resultados de las multiplicaciones
-        d = d + 1 #le sumo a d 1 para aumentar su valor y que este llegue a ser mayor o igual a b
-    print("el resultado de numero a elevado a numero b es: "+str(e))#hago la impresion
-    return e #vuelvo a e para seguir con las operaciones
-a_power_b()
+        d = d + 1 #le sumo a d 1 para aumentar su valor y que este llegue a ser menor o igual a b      
+    print("el resultado de numero a elevado al numero b es: "+str(e))#hago la impresion
+a=1
+while a<0 or a>0:
+    a=int(input("ingrese el valor de a: ")) #aqui pregunto por a
+    b=int(input("ingrese el valor de b: ")) #aqui pregunto por b
+    a_power_b(a,b)
+
+      
+
