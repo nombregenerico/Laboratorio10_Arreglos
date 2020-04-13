e388c89d6c729319a9208141a41ed706396129ec
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2020-04-12 22:52:22 -0500

    modificacion con operaciones estadisticas

--- Laboratorio10.py
+++ Laboratorio10.py
@@ -3,9 +3,9 @@
 Created on Tue Apr  7 20:22:08 2020
 
 @author: abaly
-"""
+"""  
+import numpy as np
 
-print("ingrese 0 en a para terminar")  
 def a_power_b (a,b): #creo una funcion para desarrolar el ejericio
     d = 1  #me ayuda en el proceso
     e = 1 #en esta variable voy almacenando las multiplicaciones. 
@@ -13,20 +13,32 @@ def a_power_b (a,b): #creo una funcion para desarrolar el ejericio
         e = e * a #aqui le voy agregando los resultados de las multiplicaciones
         d = d + 1 #le sumo a d 1 para aumentar su valor y que este llegue a ser menor o igual a b      
     return e
-a=1
+operan=int(input("ingrese el numero de potencias que quiere encontrar: "))
 cont=0
-cont2=0
+cont2=00
 conttotal=0
-while a<0 or a>0: #este ciclo pide numeros de entrada hasta que ingrese 0, en ese momento se detiene el algoritmo
+for i in range(0,operan,1): #este ciclo pide numeros de entrada hasta que se llegue a la cantidad de pares deseada, en ese momento se detiene el algoritmo
     a=int(input("ingrese el valor de a: ")) #aqui pregunto por a que es el numerador
     b=int(input("ingrese el valor de b: ")) #aqui pregunto por b que es el exponente 
-    conttotal=conttotal+1
-    if a_power_b(a,b)%2==0:
+    conttotal=conttotal+1 #contador de operaciones totales
+    if a_power_b(a,b)%2==0: #contador de pares
         cont=cont+1       
-    elif a_power_b(a,b)%2!=0:
+    elif a_power_b(a,b)%2!=0: #contador impares
         cont2=cont2+1 
-    print("el resultado de numero a elevado al numero b es: "+str(a_power_b(a,b)))
-print("el numero de veces en el que la potencia fue par es: "+str(cont-1))
-print("el numero de veces en el que la potencia fue impar es: "+str(cont2))
-print("el numero de veces en que se calcularon potencias fue: "+str(conttotal-1))           
-    
+    print("el resultado de numero a elevado al numero b es: "+str(a_power_b(a,b)))#impresion de resultado de cada potencia
+print("el numero de veces en el que la potencia fue par es: "+str(cont))#contador de pares
+print("el numero de veces en el que la potencia fue impar es: "+str(cont2))#contador de impares
+print("el numero de veces en que se calcularon potencias fue: "+str(conttotal))  #contador de operaciones         
+
+listaA=[0]*operan
+arregloA=np.array(listaA)
+for i in range (0,listaA):
+    print(i)
+listaB=[0]*operan
+arregloB=np.array(listaB)
+
+print
+
+
+
+
